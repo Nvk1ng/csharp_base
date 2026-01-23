9f35a3607fe12e8530d99cf0cc7bc670ab5e528c
matheus sanderhus <sanderhus15@outlook.com>
2026-01-23 13:32:18 -0300

    inteiros csharp

---
+++ Inteiros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Numero inteiros ou seja sem pontuaçao devem ser definidos pelos tipos:
+            short / ushort
+            int / uint 
+            long / ulong
+            */
+            int numero1 = 2;
+            int numero2 = -50;
+            int numero3 = 302180;
+        }
+    }
+}
