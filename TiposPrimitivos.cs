cca64ada63b41dea1a6f6927b6ef7ac6dd88d346
matheus sanderhus <sanderhus15@outlook.com>
2026-01-23 13:32:30 -0300

    Tipos primitivos csharp

---
+++ TiposPrimitivos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 25;
+            double altura = 1.75;
+            decimal preco = 99.99m;
+            char inicial = 'J';
+            bool ativo = true;
+        }
+    }
+}
