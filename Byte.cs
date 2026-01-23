cb519dfd17169d216c8e8c90d69761cbc8cd713a
matheus sanderhus <sanderhus15@outlook.com>
2026-01-23 13:32:06 -0300

    byte csharp

---
+++ Byte.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // O tipo byte é utilizado para representar um byte de fato;
+            // em diversos casos precisamos da cadeia de bytes de um arquivo por exemplo;
+            // também chamado de byte array;
+            // tambem temos o sbyte que permite valores negativos;
+
+            byte meuByte  = 127;
+        }
+    }
+}
