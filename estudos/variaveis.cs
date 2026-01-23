3cc020db0ba60f3651b022e9fa1d5a10a961a405
matheus sanderhus <sanderhus15@outlook.com>
2026-01-23 13:05:30 -0300

    pasta exercicios

---
+++ estudos/variaveis.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Uma variavel é algo que utilizamos para armazenar uma informaçao;
+            Ser variavel significa que seu valor pode ser alterado a qualquer momento;
+            Sempre que criamos uma variavel dizemos que estamos iniciando ela.
+            */
+
+            string primeiroNome = "Matheus";
+            int idade; // Correto inicia com ZERO
+            int idade = 25; // Correto inica com 25
+            var idade = 25; // Correto inicia com 25
+            var idade; // errado
+        }
+    }
+}
