bd737f85e77146f86c2ed998ae95aeba4bb096f0
matheus sanderhus <sanderhus15@outlook.com>
2026-01-24 10:47:43 -0300

    strings csharp

---
+++ Strings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeSobrenome = "Matheus Calmon";
+            var nomeDaMae = "Maria Neares";
+        }
+    }
+}
