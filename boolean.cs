9e6cce19ebd7fb56d82b7417207c76ade5d8572e
matheus sanderhus <sanderhus15@outlook.com>
2026-01-24 10:47:01 -0300

    boolean csharp

---
+++ boolean.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool pagamentoRecebido = true;
+            bool usuarioJaCadastrado = false;
+
+            var usuarioExpirado = false;
+        }
+    }
+}
