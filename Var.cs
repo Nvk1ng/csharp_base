80de0eab04436283f1e47ef50e3b410deb04149b
matheus sanderhus <sanderhus15@outlook.com>
2026-01-24 10:47:54 -0300

    var csharp

---
+++ Var.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var idade = 25;
+
+            IEnumerable<MeuTipoComplexo> aluno = new IEnumerable<MeuTipoComplexo>();
+            var aluno = new IEnumerable<MeuTipoComplexo>();
+        }
+    }
+}
