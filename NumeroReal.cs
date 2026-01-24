0b590716e1d85c0408f3907470ffbd65008fe042
matheus sanderhus <sanderhus15@outlook.com>
2026-01-24 10:47:33 -0300

    numero real csharp

---
+++ NumeroReal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var salario = 2.500;
+            float numero1 = 1.5f;
+            double numero2 = 10.5;
+            decimal numero3 = 20.22m;
+        }
+    }
+}
