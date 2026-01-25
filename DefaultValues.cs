20e4b9e542f9b8d2bd65b3e4573ecada51b54f3c
matheus sanderhus <sanderhus15@outlook.com>
2026-01-25 12:10:06 -0300

    valor default csharp

---
+++ DefaultValues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Se nenhum valor for informado, seu valor padrao sera utilizado:
+
+            int -> 0 
+            float -> 0 
+            decimal -> 0
+            bool -> false
+            char -> '\0'
+            string -> ""
+
+            */
+        }
+    }
+}
