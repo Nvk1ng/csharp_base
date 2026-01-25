3b367a5dbabf10e6652f124aaf8027dac3a8ecaa
matheus sanderhus <sanderhus15@outlook.com>
2026-01-25 12:09:39 -0300

    conversao explicita csharp

---
+++ ConversaoExplicita.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Ocorre quando os tipo nao sao compativeis;
+            e dada pelo uso do entre parenteses antes da atribuiçao;
+            Segue as mesmas regras anteriores;
+            */
+
+            int inteiro = 100;
+            uint inteiroSemSinal = (uint)inteiro; // Conversao explícita
+        }
+    }
+}
