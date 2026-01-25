1ee042e2d069e13f0d0879a2158186d788811eb8
matheus sanderhus <sanderhus15@outlook.com>
2026-01-25 12:09:21 -0300

    alias csharp

---
+++ Alias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32; // Alias
+            Int32 idade = 32; // Tipo
+        }
+    }
+}
