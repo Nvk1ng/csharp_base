e0b6208215a1d0d368c038ca66c838134752e6d6
matheus sanderhus <sanderhus15@outlook.com>
2026-01-25 12:09:53 -0300

    conversao implicita csharp

---
+++ ConversaoImplicita.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            É comum precisarmos transformar dados do tipo X em Y;
+            As conversoes implicitas sao conversoes que:
+                Podem ser executadas apenas com passagem de dados;
+                Possuem dados compatíveis.
+            */
+            float valor = 25.8f;
+            int outro = 25;
+            valor = outro;
+        }
+    }
+}
