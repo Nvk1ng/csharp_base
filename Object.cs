d930f9f2313dd68d131a17858b0633b2d2e47e79
matheus sanderhus <sanderhus15@outlook.com>
2026-01-25 12:10:30 -0300

    object csharp

---
+++ Object.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object nome = "Matheus";
+            object idade = 25;
+
+            object quantidade;
+            quantidade = 1;
+            quantidade = 2.5;
+            quantidade = "teste";
+        }
+    }
+}
