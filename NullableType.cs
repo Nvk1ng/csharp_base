0aaf48755242e944503157b9fd0724396a11f19c
matheus sanderhus <sanderhus15@outlook.com>
2026-01-25 12:10:21 -0300

    valor null csharp

---
+++ NullableType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? idade = null;
+            byte? t = 127;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(t);
+
+
+            int? num = 0;
+            Console.WriteLine(num);
+            num = null;
+            Console.WriteLine(num);
+            num = 25;
+            Console.WriteLine(num);
+        }
+    }
+}
