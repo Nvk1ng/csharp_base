c9a22ac7dd02af86d7e069498de652967e7bb93e
matheus sanderhus <sanderhus15@outlook.com>
2026-01-26 11:17:10 -0300

    parse csharp

---
+++ ConvertendoTipos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro = 100;
+            float real = 25.5f;
+
+            // real = inteiro;
+            // inteiro = (int)real;
+            // string valorReal = real.ToString();
+            // inteiro = int.Parse(real);
+            // inteiro = Convert.ToInt32(real);
+
+            // Console.WriteLine(valorReal);
+            Console.WriteLine(inteiro);
+            Console.WriteLine(Convert.ToBoolean("true"));
+        }
+    }
+}
---
+++ Parse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro = int.Parse("100");
+        }
+    }
+}
