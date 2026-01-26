18ca7c15a46317c3175b405f1b0c94f597eb70ac
matheus sanderhus <sanderhus15@outlook.com>
2026-01-26 15:30:40 -0300

    operadores aritimeticos csharp

---
+++ OperadoresAritmeticos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            soma -> +
+            subtraçao -> - 
+            multiplicaçao -> *
+            divisao -> /
+            */
+
+            int soma = 25 + 10;
+            int sub = 100 - 20;
+            int divisao = 10 / 2;
+            int mul = 10 * 2;
+
+            // Utilizamos parenteses para redefinir a ordem de execuçao
+            int x = 2 + 2 * 2;
+            int y = 2 + (2 * 2);
+            int z = (2 + 2) * 2;
+        }
+    }
+}
