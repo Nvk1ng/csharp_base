ccac5382473db685ee7c7c247d4288054a2830f0
matheus sanderhus <sanderhus15@outlook.com>
2026-01-26 15:30:50 -0300

    operadores de atribuicao  csharp

---
+++ OperadoresAtribuicao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0; // Atribuiçao
+            x += 5; // x = x + 5
+            x -= 1; // x = x - 1
+            x *= 10; // x = x * 10
+            2 /= 2; // x = x / 2
+        }
+    }
+}
