46715ce56900fec1bf0e2fe839ad33063e3c3fa9
matheus sanderhus <sanderhus15@outlook.com>
2026-01-27 10:49:30 -0300

    Operadores logicos csharp

---
+++ OperadoresLogicos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            E - and &&
+            OU - or ||
+            Negacao - not !
+            */
+
+            int x = 12;
+
+            bool entre = (x > 25) && (x < 40); // false
+            bool ou = (x > 25) || (x < 40); // true
+            bool negacao = !(x < 25); // falase
+        }
+    }
+}
