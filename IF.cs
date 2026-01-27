51b577ba37411bc0e25bf1b08e52821dd5712398
matheus sanderhus <sanderhus15@outlook.com>
2026-01-27 10:49:00 -0300

    IF csharp

---
+++ IF.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 20;
+            if(idade >= 18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+
+            int numero = 5;
+            if(numero % 2 == 0)
+            {
+                Console.WriteLine("par");
+            }
+            else
+            {
+                Console.WriteLine("ímpar");
+            }
+
+            int nota = 75;
+            if(nota >= 90)
+            {
+                Console.WriteLine("A");
+            }else if(nota >= 70){
+                Console.WriteLine("B");
+            }else if(nota >= 50)
+            {
+                Console.WriteLine("C");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
