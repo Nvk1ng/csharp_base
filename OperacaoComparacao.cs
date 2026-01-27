1caf0e357fcbe518bbc7979d45cc9dab76237c7e
matheus sanderhus <sanderhus15@outlook.com>
2026-01-27 10:49:16 -0300

    Operadores comparacao csharp

---
+++ OperacaoComparacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // A comparaçao sempre retorna um true ou false;
+
+            int x = 25;
+
+            x == 0; // false
+            x != 0; // true
+            x > 0; // true
+            x < 0; // false
+            x <= 0; // false
+            x >= 0; // true
+        }
+    }
+}
