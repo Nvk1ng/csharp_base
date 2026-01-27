065d60e966423bdc9c90b9c4d34815c55accf7c2
matheus sanderhus <sanderhus15@outlook.com>
2026-01-27 13:22:44 -0300

    exemplos IF csharp

---
+++ UtilizandoIF.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 18;
+            int maioridade = 21;
+            int idadeMaximo = 65;
+
+            if(idade >= maioridade)
+            {
+                Console.WriteLine("É diferente");
+            }
+            else if(idade >= 25)
+            {
+                Console.WriteLine("É igual");
+            }
+            else if(idade >= 32)
+            {
+                Console.WriteLine("É igual");
+            }
+            else if(idade >= 28)
+            {
+                Console.WriteLine("É igual");
+            }
+            else
+            {
+                Console.WriteLine("É igual");
+            }
+        }
+    }
+}
