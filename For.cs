9ee5e29dbf0cda01ac4b1bec1eaa70291d81a247
matheus sanderhus <sanderhus15@outlook.com>
2026-01-28 14:59:00 -0300

    for csharp

---
+++ For.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for(var i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for(var i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for(var i = 5; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
