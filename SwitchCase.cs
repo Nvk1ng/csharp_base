8f82e723b86f629e6a510a6ac7dce4ae4d605c56
matheus sanderhus <sanderhus15@outlook.com>
2026-01-28 14:59:46 -0300

    switch  case csharp

---
+++ SwitchCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Utilizado quando temos muitas decisoes
+
+            string valor = "matheus";
+            switch (valor)
+            {
+                case "joao": Console.WriteLine("Olá joao"); break;
+                case "marcelo": Console.WriteLine("Olá marcelo"); break;
+                case "matheus": Console.WriteLine("Olá matheus"); break;
+                default: Console.WriteLine("Nao encontrei"); break;
+            }
+        }
+    }
+}
