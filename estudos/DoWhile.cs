a177eebc1c0b56217cef603fd9c2721bfbcc7a17
matheus sanderhus <sanderhus15@outlook.com>
2026-01-28 15:09:34 -0300

    adicionando a pasta de exemplos

---
+++ estudos/DoWhile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var valor =0;
+
+            do
+            {
+                Console.WriteLine("Teste");
+                valor ++;
+            }while(valor <5);
+        }
+    }
+}
