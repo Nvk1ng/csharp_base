5acdb48290863aadd54051736b413375ffc5fb31
matheus sanderhus <sanderhus15@outlook.com>
2026-01-29 11:30:00 -0300

    metodos e funcoes

---
+++ MetodosFuncoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo("C# é bom!");
+
+            var nome = RetornaNome("Matheus", "Sanderhus");
+            Console.WriteLine(nome);
+        }
+
+        // Definiçao do metodo
+        static void MeuMetodo(string parametro)
+        {
+            Console.WriteLine(parametro);
+        }
+
+        static string RetornaNome(string nome, string sobrenome)
+        {
+            return nome + " " + sobrenome;
+        }
+    }
+}
