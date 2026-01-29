d242cd38fa0600ac7414a6d649fafa76eb826b19
matheus sanderhus <sanderhus15@outlook.com>
2026-01-29 11:29:46 -0300

    funcoes na pratica

---
+++ FuncoesNaPratica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+            string nome = RetornaNome("Andre", "baltiere", 30);
+            Console.WriteLine(nome);        
+            }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("Meu Metodo");
+        }
+
+        static string RetornaNome(string nome, string sobrenome, int idade)
+        {
+            return nome + " " + sobrenome + " " + idade.ToString();
+        }
+    }
+}
