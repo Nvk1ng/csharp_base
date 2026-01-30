0ca409098dc49dd6e7e30790004deedd7f08bb26
matheus sanderhus <sanderhus15@outlook.com>
2026-01-30 13:51:26 -0300

    structs exemplos csharp

---
+++ StructsExemplo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Product mouse = new Product();
+            var mouse = new Product(1, "mouse gamer", 20.99);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+        }
+    }
+    struct Product
+    {
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id;
+        public string Name; 
+        public double Price;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+}   
