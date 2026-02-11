879222a692b3c209df50f67077e41694226d6107
matheus sanderhus <sanderhus15@outlook.com>
2026-02-11 11:54:12 -0300

    exemplos de interpolacao

--- InterpolacaoString.cs
+++ InterpolacaoString.cs
@@ -6,6 +6,12 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
+            var price = 10.2;
+            //var text = "O preço do produto é " + price + " apenas na promoçao!";
+            //var text = string.Format("O preço do produto é {0} apenas na promoçao", price);
+            var text = $"o preço do produto é {price} apenas na promocao";
+
+            Console.WriteLine(text);
         }
     }
 }
