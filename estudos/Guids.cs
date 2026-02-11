079e03a77377d9ac257030e1f7396e1ea7b7e6bf
matheus sanderhus <sanderhus15@outlook.com>
2026-02-11 11:40:38 -0300

    Adicionando guid

--- estudos/Guids.cs
+++ estudos/Guids.cs
@@ -6,6 +6,11 @@ namespace MeuApp
     {
         static void Main(string[] args)
         {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            id = new Guid("0f1d61e5-b605-4258-898d-bfe26f64b3dc");
+            Console.WriteLine(id);
         }
     }
 }
