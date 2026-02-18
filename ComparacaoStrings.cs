2dd82a31110aa4d6f7ef2479614d1354063c283c
matheus sanderhus <sanderhus15@outlook.com>
2026-02-18 14:38:29 -0300

    Comparaçao de strings

---
+++ ComparacaoStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Testando";
+            var texto = "Esse texto é um teste";
+            
+            Console.WriteLine(texto.CompareTo("Testando"));
+            Console.WriteLine(texto.CompareTo("testando"));
+
+            Console.WriteLine(texto.Contains("teste"));
+            Console.WriteLine(texto.Contains("Teste"));
+            Console.WriteLine(texto.Contains("Teste", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
