6f7bdeb3b8126a8c0243ce6a0aed84397bc560ae
matheus sanderhus <sanderhus15@outlook.com>
2026-02-18 14:39:04 -0300

    Startwith and endswith strings

---
+++ StartWithEndsWith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste";
+
+            Console.WriteLine(texto.StartsWith("Este"));
+            Console.WriteLine(texto.StartsWith("este"));
+
+            Console.WriteLine(texto.EndsWith("teste"));
+            Console.WriteLine(texto.EndsWith("teste"));
+        }
+    }
+}
