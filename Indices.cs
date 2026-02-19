232d63468f61c4959fbb8ff2ceed5ba0ece81984
matheus sanderhus <sanderhus15@outlook.com>
2026-02-19 14:23:48 -0300

    Indices em csharp

---
+++ Indices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este esto é um teste";
+            Console.WriteLine(texto.IndexOf("teste"));
+            Console.WriteLine(texto.LastIndexOf("s"));
+        }
+    }
+}
