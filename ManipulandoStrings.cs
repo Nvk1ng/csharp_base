30e2b30860a92570bc9cf636894fd66b40204b6b
matheus sanderhus <sanderhus15@outlook.com>
2026-02-19 14:24:04 -0300

    Manipulando strings em csharp

---
+++ ManipulandoStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Olá, Mundo!";
+            Console.WriteLine(texto.Replace("Olá", "Oi"));
+            Console.WriteLine(texto.Replace("o", "x"));
+
+            var divisao = texto.Split(" ");
+            Console.WriteLine(divisao[0]);
+            Console.WriteLine(divisao[1]);
+
+            var resultado = texto.Substring(4, 5);
+            Console.WriteLine(resultado);
+        }
+    }
+}
