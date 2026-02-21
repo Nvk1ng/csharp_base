35a242294a2f077079c98adee6903412f12a3b58
matheus sanderhus <sanderhus15@outlook.com>
2026-02-20 21:25:48 -0300

    stringbuilder

---
+++ StringBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace MeuApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var  texto = new StringBuilder();
+            texto.Append("Este texto é um teste");
+            texto.Append("é um teste");
+            texto.Append("Este texto");
+            texto.Append("Este texto é um teste");
+
+        }
+    }
+}
